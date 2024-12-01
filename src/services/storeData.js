788de9fabf47529cca8ac21425530e16d60d2bc4
ahmadhidayat22 <ahmadnurhidayat271@gmail.com>
2024-12-02 01:05:08 +0800

    update

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,9 +1,7 @@
 const { Firestore } = require('@google-cloud/firestore');
 const projectId = process.env.PROJECT_ID;
 async function storeData(id, data) {
-    const db = new Firestore({
-        projectId
-    });
+    const db = new Firestore();
    
     const predictCollection = db.collection('prediction');
     return predictCollection.doc(id).set(data);
